69bc3a3c962832da829cb99db782a3cee701fe8c
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2026-02-27 14:47:13 +0900

    feat: category 추가 기능

--- src/main/java/com/delivery/project/category/controller/CategoryController.java
+++ src/main/java/com/delivery/project/category/controller/CategoryController.java
@@ -1,9 +1,9 @@
 package com.delivery.project.category.controller;
 
+import com.delivery.project.category.dto.CategoryRequestDto;
 import com.delivery.project.category.service.CategoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/categories")
@@ -15,6 +15,11 @@ public class CategoryController {
     // TODO: GET    /api/v1/categories       - 카테고리 목록 조회
     // TODO: GET    /api/v1/categories/{id}  - 카테고리 단건 조회
     // TODO: POST   /api/v1/categories       - 카테고리 등록
+    @PostMapping
+    public void insertCategory(@RequestBody CategoryRequestDto requestDto) {
+        categoryService.insertCategory(requestDto);
+    }
+
     // TODO: PUT    /api/v1/categories/{id}  - 카테고리 수정
     // TODO: DELETE /api/v1/categories/{id}  - 카테고리 삭제
 }
--- src/main/java/com/delivery/project/category/service/CategoryService.java
+++ src/main/java/com/delivery/project/category/service/CategoryService.java
@@ -1,10 +1,14 @@
 package com.delivery.project.category.service;
 
+import com.delivery.project.category.dto.CategoryRequestDto;
+import com.delivery.project.category.entity.Category;
 import com.delivery.project.category.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @Service
@@ -16,7 +20,20 @@ public class CategoryService {
 
     // TODO: 카테고리 목록 조회
     // TODO: 카테고리 단건 조회
+
     // TODO: 카테고리 등록 (MANAGER+)
+    @Transactional
+    public void insertCategory(CategoryRequestDto requestDto) {
+        Category category = Category.builder()
+                .name(requestDto.getName())
+                .isActive(true)
+                .createdAt(LocalDateTime.now())
+                .createdBy("MANAGER")
+                .build();
+
+        categoryRepository.save(category);
+    }
+
     // TODO: 카테고리 수정 (MANAGER+)
     // TODO: 카테고리 삭제 Soft Delete (MANAGER+)
 }
