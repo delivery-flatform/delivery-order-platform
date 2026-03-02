ddfb02bfc8ffc62aa772ee80a7306082e5c4c514
ParkSeYoung128 <tpdudtpdud128@gmail.com>
2026-03-02 22:38:45 +0900

    JWT 인증 객체 생성 파라미터 변경 & 회원 단건 조회 기능 구현

--- src/main/java/com/delivery/project/user/controller/UserController.java
+++ src/main/java/com/delivery/project/user/controller/UserController.java
@@ -1,7 +1,14 @@
 package com.delivery.project.user.controller;
 
+import com.delivery.project.global.response.ApiResponse;
+import com.delivery.project.global.security.UserDetailsImpl;
+import com.delivery.project.user.dto.response.UserResponseDto;
 import com.delivery.project.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +20,18 @@ public class UserController {
     private final UserService userService;
 
     // TODO: GET  /api/v1/users          - 회원 목록 조회 (MANAGER+)
-    // TODO: GET  /api/v1/users/{username} - 회원 단건 조회
+    // 회원 단건 조회
+    @GetMapping("/{username}")
+    public ResponseEntity<ApiResponse<UserResponseDto>> getUser(
+            @PathVariable String username,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {  // SecurityContext에서 자동 주입
+        return ResponseEntity.ok(
+                ApiResponse.success(
+                        userService.getUser(username, userDetails.getUsername(), userDetails.getUser().getRole())
+                )
+        );
+    }
+
     // TODO: PUT  /api/v1/users/{username} - 회원 정보 수정
     // TODO: DELETE /api/v1/users/{username} - 회원 삭제
 }
--- src/main/java/com/delivery/project/user/service/UserService.java
+++ src/main/java/com/delivery/project/user/service/UserService.java
@@ -1,5 +1,10 @@
 package com.delivery.project.user.service;
 
+import com.delivery.project.global.exception.CustomException;
+import com.delivery.project.global.exception.ErrorCode;
+import com.delivery.project.user.dto.response.UserResponseDto;
+import com.delivery.project.user.entity.User;
+import com.delivery.project.user.entity.UserRole;
 import com.delivery.project.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +20,22 @@ public class UserService {
     private final UserRepository userRepository;
 
     // TODO: 회원 목록 조회
-    // TODO: 회원 단건 조회
+
+    // 회원 단건 조회 (본인 또는 MANAGER+)
+    public UserResponseDto getUser(String username, String currentUsername, UserRole currentRole) {
+        boolean isSelf = username.equals(currentUsername);
+        boolean isManagerOrAbove = currentRole == UserRole.MANAGER || currentRole == UserRole.MASTER;
+
+        if (!isSelf && !isManagerOrAbove) {
+            throw new CustomException(ErrorCode.FORBIDDEN);
+        }
+
+        User user = userRepository.findByUsernameAndDeletedAtIsNull(username)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        return new UserResponseDto(user);
+    }
+
     // TODO: 회원 정보 수정
     // TODO: 회원 삭제 (Soft Delete)
     // TODO: 권한 변경 (MANAGER+)
