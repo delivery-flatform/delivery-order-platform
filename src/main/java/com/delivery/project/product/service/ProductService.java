c1d9498b7c1d0192a239625473e987ef97b61a83
yeonheeMoon <xsss7@naver.com>
2026-03-11 20:25:59 +0900

    refactor : 구조 개선 (@PreAuthorize 위치 변경)

--- src/main/java/com/delivery/project/product/service/ProductService.java
+++ src/main/java/com/delivery/project/product/service/ProductService.java
@@ -15,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -69,6 +70,7 @@ public class ProductService {
     }
 
     // TODO: 상품 등록
+    @PreAuthorize("hasAnyRole('OWNER')")
     public UUID createProduct(ProductCreateRequest request, String userName) {
 
         Store store = storeRepository.findById(request.getStoreId())
@@ -90,6 +92,7 @@ public class ProductService {
     }
 
     // TODO: 상품 수정
+    @PreAuthorize("hasAnyRole('MANAGER', 'MASTER', 'OWNER')")
     public void updateProduct(UUID productId, ProductUpdateRequest request, String userName) {
 
         Product product = getActiveProduct(productId);
@@ -103,6 +106,7 @@ public class ProductService {
     }
 
     // TODO: 상품 삭제 Soft Delete
+    @PreAuthorize("hasAnyRole('MANAGER', 'MASTER', 'OWNER')")
     public void deleteProduct(UUID productId, String userName) {
 
         Product product = getActiveProduct(productId);
@@ -111,6 +115,7 @@ public class ProductService {
     }
 
     // TODO: 상품 숨김 처리
+    @PreAuthorize("hasAnyRole('MANAGER', 'MASTER', 'OWNER')")
     public void hideProduct(UUID productId, String userName) {
 
         Product product = getActiveProduct(productId);
@@ -119,6 +124,7 @@ public class ProductService {
     }
 
     // TODO: 상품 숨김 해제
+    @PreAuthorize("hasAnyRole('MANAGER', 'MASTER', 'OWNER')")
     public void unhideProduct(UUID productId, String userName) {
 
         Product product = getActiveProduct(productId);
